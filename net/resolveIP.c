2cd7b81e25a73515449358e69e3740a9348f151b
Develon Ubuntu16.04 Desktop <no>
2019-03-07 06:45:26 -0800

    正常提交

--- net/resolveIP.c
+++ net/resolveIP.c
@@ -11,6 +11,8 @@ resolveIP(const char *domain, int type, void *ip) {
 	if (type != AF_INET && type != AF_INET6)
 		return -1;
 	struct hostent *ent = gethostbyname2(domain, type);
+	if (ent == NULL)
+		return -1;
 	if (type == AF_INET)
 		*(struct in_addr *)ip = *(struct in_addr*)ent->h_addr;
 	else
